3590ce4f35af767a5f9b2f9bce3850e5c379379c
ra0sms <ra0sms@bk.ru>
2025-03-17 00:31:26 +0000

    correct install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-echo "Updating..."
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}Updating...${NC}"
 sudo apt-get update
 
-echo "Upgrading..."
+echo -e "${GREEN}Upgrading...${NC}"
 sudo apt-get upgrade -y
 
-echo "Installing new packages..."
+echo -e "${GREEN}Installing new packages...${NC}"
 sudo apt-get install -y git make gcc python3 python3-pip ser2net swig python3-dev python3-setuptools 
 
-echo "Cleaning..."
+echo -e "${GREEN}Cleaning...${NC}"
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 
-echo "Done"
+echo -e "${GREEN}Done${NC}"
