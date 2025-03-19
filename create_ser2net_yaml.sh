8a1d2726f86508d7a4d2bf756d4a800b6e309e32
ra0sms <ra0sms@bk.ru>
2025-03-19 08:22:30 +0000

    add create_ser2net_yaml.sh

---
+++ create_ser2net_yaml.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Получаем текущий IP-адрес компьютера
+CURRENT_IP=$(hostname -I | awk '{print $1}')
+
+# Шаблон конфигурации ser2net
+CONFIG_TEMPLATE="%YAML 1.1
+---
+# This is a ser2net configuration file, tailored to be rather
+# simple.
+#
+# Find detailed documentation in ser2net.yaml(5)
+# A fully featured configuration file is in
+# /usr/share/doc/ser2net/examples/ser2net.yaml.gz
+# 
+# If you find your configuration more useful than this very simple
+# one, please submit it as a bugreport
+
+define: &banner \\r\\nser2net port \\p device \\d [\\B] (Debian GNU/Linux)\\r\\n\\r\\n
+
+connection: &con0096
+    accepter: udp,${CURRENT_IP},3001
+    enable: on
+    options:
+      banner: *banner
+      kickolduser: true
+      telnet-brk-on-sync: true
+    connector: serialdev,
+              /dev/ttyUSB0,
+              19200n81,local
+
+connection: &con1096
+    accepter: udp,${CURRENT_IP},3002
+    enable: on
+    options:
+      banner: *banner
+      kickolduser: true
+      telnet-brk-on-sync: true
+    connector: serialdev,
+              /dev/ttyS1,
+              19200n81,local
+"
+
+# Сохраняем конфигурацию в файл
+echo "$CONFIG_TEMPLATE" > /etc/ser2net.yaml
+sudo service ser2net restart
+
+echo "File ser2net_config.yaml was created with IP: ${CURRENT_IP}"
