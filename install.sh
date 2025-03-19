4866e6c101ae44abba88b58c03052675afbce436
ra0sms <ra0sms@bk.ru>
2025-03-19 12:43:07 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,27 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 echo -e "${GREEN}Updating...${NC}"
-sudo apt-get update
+apt-get update
 
 echo -e "${GREEN}Upgrading...${NC}"
-sudo apt-get upgrade -y
+apt-get upgrade -y
 
 echo -e "${GREEN}Installing new packages...${NC}"
-sudo apt-get install -y git make gcc python3 python3-pip ser2net swig python3-dev python3-setuptools 
+apt-get install -y git make gcc python3 python3-pip ser2net swig python3-dev python3-setuptools 
 
 echo -e "${GREEN}Cleaning...${NC}"
-sudo apt-get autoremove -y
-sudo apt-get autoclean -y
+apt-get autoremove -y
+apt-get autoclean -y
+
+echo -e "${GREEN}Config ser2net...${NC}"
+./create_ser2net_yaml.sh
+
+echo -e "${GREEN}Config ptt_server.service...${NC}"
+cp ./ptt_server.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl start ptt_server.service
+systemctl enable ptt_server.service
+systemctl status ptt_server.service
+echo -e "${GREEN}ptt_server.service started and enabled${NC}"
 
 echo -e "${GREEN}Done${NC}"
