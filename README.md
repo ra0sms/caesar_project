9d5c68e8c594997975221a1e93585648237dd21e
ra0sms <ra0sms@bk.ru>
2025-03-25 02:09:25 +0000

    add clien ping responce service

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 * `UDP 5000` - Audio stream
 * `UDP 5001` - PTT control
 * `UDP 5002` - CW control
+* `UDP 5003` - check connection from server to client
 * `UDP 3001` - cat control via USB-TTL (`ttyUSB0`)
 * `UDP 3002` - cat control via internal serial port (`ttyS1`)
 
@@ -14,7 +15,7 @@ Serial port - 19200 kb/s, 8N1
 
 ## Files
 
-`install_server.sh` - install all dependencies
+`install_server.sh` - install all dependencies for server
 
 
 ## Installation
---
+++ client_ping_responce.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import socket
+
+UDP_PORT = 5003
+RESPONSE_PHRASE = b"PING_RESPONSE"
+
+def run_udp_responder():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', UDP_PORT))
+    
+    print(f"UDP responder started on port {UDP_PORT}")
+    
+    try:
+        while True:
+            data, addr = sock.recvfrom(1024)
+            
+            # Если получен запрос "PING_REQUEST"
+            if data == b"PING_REQUEST":
+                print(f"Received ping from {addr[0]}")
+                sock.sendto(RESPONSE_PHRASE, addr)
+    except KeyboardInterrupt:
+        print("\nServer stopped")
+    finally:
+        sock.close()
+
+if __name__ == "__main__":
+    run_udp_responder()
