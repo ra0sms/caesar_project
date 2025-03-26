030f9207f349d846e91c482a054037942925e1eb
ra0sms <ra0sms@bk.ru>
2025-03-26 05:03:20 +0000

    fix sound

--- audio_server_on_client.sh
+++ audio_server_on_client.sh
@@ -14,11 +14,11 @@ if [[ -z "$IP_ADDRESS" ]]; then
 fi
 
 # Audio settings
-/usr/bin/amixer -c 0 cset numid=7 4
+/usr/bin/amixer -c 0 cset numid=7 6
 /usr/bin/amixer -c 0 cset numid=18 on
-/usr/bin/amixer -c 0 cset numid=8 1
+/usr/bin/amixer -c 0 cset numid=8 6
 /usr/bin/amixer -c 0 cset numid=13 off
 
 # Audio stream
-gst-launch-1.0   alsasrc device=hw:0 !   audioconvert !   audioresample quality=1 !   capsfilter caps="audio/x-raw,rate=48000,channels=1,format=S16LE" !   opusenc bitrate=24000 frame-size=10 complexity=1 !   rtpopuspay !   udpsink host=$IP_ADDRESS port=5002 sync=false
+gst-launch-1.0   alsasrc device=hw:0 !   audioconvert !   audioresample !   capsfilter caps="audio/x-raw,rate=48000,channels=1,format=S16LE" !   opusenc bitrate=24000 frame-size=10 complexity=2 !   rtpopuspay !   udpsink host=$IP_ADDRESS port=5002 sync=false
 
