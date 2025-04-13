0021dddae1014b25c386e81cdc028ec9e822fc3b
ra0sms <ra0sms@bk.ru>
2025-04-13 20:34:59 +0800

    fix audio stream

--- audio_server_on_client.sh
+++ audio_server_on_client.sh
@@ -20,5 +20,5 @@ fi
 /usr/bin/amixer -c 0 cset numid=13 off
 
 # Audio stream
-gst-launch-1.0   alsasrc device=hw:0 !   audioconvert !   audioresample !   capsfilter caps="audio/x-raw,rate=48000,channels=1,format=S16LE" !   opusenc bitrate=24000 frame-size=10 complexity=2 !   rtpopuspay !   udpsink host=$IP_ADDRESS port=5002 sync=false
+gst-launch-1.0   alsasrc device=hw:0 buffer-time=300000 latency-time=20000 !   audioconvert !   audioresample !   capsfilter caps="audio/x-raw,rate=48000,channels=1,format=S16LE" !   opusenc bitrate=24000 frame-size=20 complexity=0 !   rtpopuspay !   udpsink host=$IP_ADDRESS port=5002 sync=false
 
