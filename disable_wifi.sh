43f4878db97be02b970b2fddd4d84a75d8b3c7b2
ra0sms <ra0sms@bk.ru>
2025-05-21 16:24:52 +0800

    fix disable_wifi

--- disable_wifi.sh
+++ disable_wifi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "${GREEN}Disable wi-fi module...${NC}"
-modprobe -r xradio_wlan || { echo -e "${RED}Failed to disable wi-fi  module${NC}"; exit 1; }
-echo "blacklist xradio_wlan" | tee -a /etc/modprobe.d/blacklist.conf | { echo -e "${RED}Failed to disable wi-fi  module${NC}"; exit 1; }
-update-initramfs -u || { echo -e "${RED}Failed to update initramfs${NC}"; exit 1; }
+sudo modprobe -r xradio_wlan || { echo -e "${RED}Failed to disable wi-fi  module${NC}"; exit 1; }
+echo "blacklist xradio_wlan" | sudo tee -a /etc/modprobe.d/blacklist.conf | { echo -e "${RED}Failed to disable wi-fi  module${NC}"; exit 1; }
+sudo update-initramfs -u || { echo -e "${RED}Failed to update initramfs${NC}"; exit 1; }
 echo -e "${GREEN}wi-fi module disabled${NC}"
