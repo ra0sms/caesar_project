355094626f5c69d681b946a4221942140cf2061a
ra0sms <ra0sms@bk.ru>
2025-05-21 16:29:11 +0800

    remove disable_wifi, add its in install scripts

--- disable_wifi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -e "${GREEN}Disable wi-fi module...${NC}"
-modprobe -r xradio_wlan || { echo -e "${RED}Failed to disable wi-fi  module${NC}"; exit 1; }
-echo "blacklist xradio_wlan" | tee -a /etc/modprobe.d/blacklist.conf || { echo -e "${RED}Failed to disable wi-fi  module${NC}"; exit 1; }
-update-initramfs -u || { echo -e "${RED}Failed to update initramfs${NC}"; exit 1; }
-echo -e "${GREEN}wi-fi module disabled${NC}"
