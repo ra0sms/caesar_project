9bcca1cc6e3c51c987646df6eea9ada4d22167f5
ra0sms <ra0sms@bk.ru>
2025-09-24 15:24:24 +0800

    add alsactl store

--- restart_services_on_server.sh
+++ restart_services_on_server.sh
@@ -4,3 +4,4 @@ systemctl restart audio_client_on_server.service
 systemctl restart check_client.service
 systemctl restart server_ping_responce.service
 systemctl restart ptt_server.service
+alsactl store
