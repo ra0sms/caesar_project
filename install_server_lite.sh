7fed020cb96417cae3d80c481a58c32d0ea0d879
ra0sms <ra0sms@bk.ru>
2025-09-26 11:13:44 +0800

    fix install

--- install_server_lite.sh
+++ install_server_lite.sh
@@ -1,116 +1,89 @@
 #!/bin/bash
-# Install all needed packages and set up environment for SERVER part
+# Install required packages and configure environment for SERVER part
 
+# ANSI color codes
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-# Function to handle errors
+
 handle_error() {
-    echo -e "${RED}Error occurred in command: $1${NC}"
+    echo -e "${RED}Error in command: $1${NC}"
     echo -e "${RED}Exiting script...${NC}"
     exit 1
 }
 
-# Enable error trapping
 set -e
 trap 'handle_error "$BASH_COMMAND"' ERR
 
-echo -e "${GREEN}Updating...${NC}"
+echo -e "${GREEN}Updating package lists...${NC}"
 apt-get update || { echo -e "${RED}Failed to update packages${NC}"; exit 1; }
 
-echo -e "${GREEN}Installing new packages...${NC}"
-apt-get install -y make gcc python3 python3-pip swig python3-dev python3-setuptools mc socat avahi-daemon python3-flask python3-waitress || { echo -e "${RED}Failed to install basic packages${NC}"; exit 1; }
-apt-get install -y gstreamer1.0-plugins-base gstreamer1.0-alsa gstreamer1.0-tools gstreamer1.0-plugins-good || { echo -e "${RED}Failed to install gstreamer good packages${NC}"; exit 1; }
-apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav || { echo -e "${RED}Failed to install gstreamer other packages${NC}"; exit 1; }
+echo -e "${GREEN}Installing required packages...${NC}"
+apt-get install -y \
+  make gcc python3 python3-pip swig python3-dev python3-setuptools mc socat avahi-daemon \
+  python3-flask python3-waitress gstreamer1.0-plugins-base gstreamer1.0-alsa \
+  gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
+  gstreamer1.0-plugins-ugly gstreamer1.0-libav || 
+  { echo -e "${RED}Failed to install packages${NC}"; exit 1; }
 
-echo -e "${GREEN}Cleaning...${NC}"
-apt-get autoremove -y || { echo -e "${RED}Failed to autoremove packages${NC}"; exit 1; }
-apt-get autoclean -y || { echo -e "${RED}Failed to autoclean packages${NC}"; exit 1; }
+echo -e "${GREEN}Cleaning up...${NC}"
+apt-get autoremove -y && apt-get autoclean -y
 usermod -a -G dialout pi || { echo -e "${RED}Failed to modify user groups${NC}"; exit 1; }
 
-echo -e "${GREEN}Set hostname...${NC}"
+echo -e "${GREEN}Setting hostname...${NC}"
 hostnamectl set-hostname caesar-server || { echo -e "${RED}Failed to set hostname${NC}"; exit 1; }
-echo -e "${GREEN}Hostname changed...${NC}"
 
-echo -e "${GREEN}Config hardware...${NC}"
-rm -f /boot/armbianEnv.txt || { echo -e "${RED}Failed to remove old armbianEnv.txt${NC}"; exit 1; }
-cp ./armbianEnv_lite.txt /boot/armbianEnv.txt || { echo -e "${RED}Failed to copy armbianEnv.txt${NC}"; exit 1; }
+echo -e "${GREEN}Configuring hardware...${NC}"
+rm -f /boot/armbianEnv.txt && cp ./armbianEnv_lite.txt /boot/armbianEnv.txt
 
-echo -e "${GREEN}Create server_ip.cfg and client_ip.cfg...${NC}"
-echo "10.0.0.2" > ./server_ip.cfg || { echo -e "${RED}Failed to create server_ip.cfg${NC}"; exit 1; }
-echo "10.0.0.3" > ./client_ip.cfg || { echo -e "${RED}Failed to create client_ip.cfg${NC}"; exit 1; }
-echo -e "${GREEN}IP addresses successfully written to config files${NC}"
+echo -e "${GREEN}Creating IP configuration files...${NC}"
+cat <<EOF > server_ip.cfg || { echo -e "${RED}Failed to create server_ip.cfg${NC}"; exit 1; }
+10.0.0.2
+EOF
 
-echo -e "${GREEN}Installing wiringPO...${NC}"
-cd wiringOP/ || { echo -e "${RED}Failed to enter wiringOP directory${NC}"; exit 1; }
-./build clean || { echo -e "${RED}Failed to clean wiringOP${NC}"; exit 1; }
-./build || { echo -e "${RED}Failed to build wiringOP${NC}"; exit 1; }
-cd ../
-cd wiringOP-Python/ || { echo -e "${RED}Failed to enter wiringOP-Python directory${NC}"; exit 1; }
-python3 generate-bindings.py > bindings.i || { echo -e "${RED}Failed to generate bindings${NC}"; exit 1; }
-python3 setup.py install || { echo -e "${RED}Failed to install wiringOP-Python${NC}"; exit 1; }
-gpio readall || { echo -e "${RED}Failed to run gpio readall${NC}"; exit 1; }
-echo -e "${GREEN}wiringPO installed...${NC}"
+cat <<EOF > client_ip.cfg || { echo -e "${RED}Failed to create client_ip.cfg${NC}"; exit 1; }
+10.0.0.3
+EOF
 
-echo -e "${GREEN}Config ptt_server.service...${NC}"
-cd ../
-chmod +x ./ptt_server.py
-cp ./ptt_server.service /etc/systemd/system/ || { echo -e "${RED}Failed to copy ptt_server.service${NC}"; exit 1; }
-systemctl daemon-reload || { echo -e "${RED}Failed to reload systemd daemon${NC}"; exit 1; }
-systemctl start ptt_server.service || { echo -e "${RED}Failed to start ptt_server.service${NC}"; exit 1; }
-systemctl enable ptt_server.service || { echo -e "${RED}Failed to enable ptt_server.service${NC}"; exit 1; }
-echo -e "${GREEN}ptt_server.service started and enabled${NC}"
+/usr/sbin/alsactl -f /var/lib/alsa/asound.state store
 
-echo -e "${GREEN}Config check_client.service...${NC}"
-cp ./check_client.service /etc/systemd/system/ || { echo -e "${RED}Failed to copy check_client.service${NC}"; exit 1; }
-systemctl daemon-reload || { echo -e "${RED}Failed to reload systemd daemon${NC}"; exit 1; }
-systemctl start check_client.service || { echo -e "${RED}Failed to start check_client.service${NC}"; exit 1; }
-systemctl enable check_client.service || { echo -e "${RED}Failed to enable check_client.service${NC}"; exit 1; }
-echo -e "${GREEN}check_client.service started and enabled${NC}"
+echo -e "${GREEN}Installing wiringOP...${NC}"
+pushd wiringOP/ || { echo -e "${RED}Failed to enter wiringOP directory${NC}"; exit 1; }
+./build clean && ./build
+popd
 
-echo -e "${GREEN}Config audio_server.service...${NC}"
-cp ./audio_server.service /etc/systemd/system/ || { echo -e "${RED}Failed to copy audio_server.service${NC}"; exit 1; }
-systemctl daemon-reload || { echo -e "${RED}Failed to reload systemd daemon${NC}"; exit 1; }
-systemctl start audio_server.service || { echo -e "${RED}Failed to start audio_server.service${NC}"; exit 1; }
-systemctl enable audio_server.service || { echo -e "${RED}Failed to enable audio_server.service${NC}"; exit 1; }
-echo -e "${GREEN}audio_server.service started and enabled${NC}"
+pushd wiringOP-Python/ || { echo -e "${RED}Failed to enter wiringOP-Python directory${NC}"; exit 1; }
+python3 generate-bindings.py > bindings.i && python3 setup.py install
+gpio readall
+popd
 
-echo -e "${GREEN}Config audio_client_on_server.service...${NC}"
-cp ./audio_client_on_server.service /etc/systemd/system/ || { echo -e "${RED}Failed to copy audio_client_on_server.service${NC}"; exit 1; }
-systemctl daemon-reload || { echo -e "${RED}Failed to reload systemd daemon${NC}"; exit 1; }
-systemctl start audio_client_on_server.service || { echo -e "${RED}Failed to start audio_client_on_server.service${NC}"; exit 1; }
-systemctl enable audio_client_on_server.service || { echo -e "${RED}Failed to enable audio_client_on_server.service${NC}"; exit 1; }
-echo -e "${GREEN}audio_client_on_server.service started and enabled${NC}"
+setup_service() {
+  local service_name=$1
+  cp ./${service_name}.service /etc/systemd/system/ || 
+    { echo -e "${RED}Failed to copy ${service_name}.service${NC}"; exit 1; }
+  systemctl daemon-reload
+  systemctl start ${service_name}.service || 
+    { echo -e "${RED}Failed to start ${service_name}.service${NC}"; exit 1; }
+  systemctl enable ${service_name}.service || 
+    { echo -e "${RED}Failed to enable ${service_name}.service${NC}"; exit 1; }
+  echo -e "${GREEN}${service_name}.service configured successfully${NC}"
+}
 
-echo -e "${GREEN}Config server_ping_responce.service...${NC}"
-cp ./server_ping_responce.service /etc/systemd/system/ || { echo -e "${RED}Failed to copy server_ping_responce.service${NC}"; exit 1; }
-systemctl daemon-reload || { echo -e "${RED}Failed to reload systemd daemon${NC}"; exit 1; }
-systemctl start server_ping_responce.service || { echo -e "${RED}Failed to start server_ping_responce.service${NC}"; exit 1; }
-systemctl enable server_ping_responce.service || { echo -e "${RED}Failed to enable server_ping_responce.service${NC}"; exit 1; }
-echo -e "${GREEN}server_ping_responce.service started and enabled${NC}"
+services=("ptt_server" "check_client" "audio_server" "audio_client_on_server" 
+          "server_ping_responce" "web_config_server" "alsa_restore")
 
-echo -e "${GREEN}Disable wi-fi module...${NC}"
-modprobe -r xradio_wlan || { echo -e "${RED}Failed to disable wi-fi  module${NC}"; exit 1; }
-echo "blacklist xradio_wlan" | tee -a /etc/modprobe.d/blacklist.conf || { echo -e "${RED}Failed to disable wi-fi  module${NC}"; exit 1; }
-update-initramfs -u || { echo -e "${RED}Failed to update initramfs${NC}"; exit 1; }
-echo -e "${GREEN}wi-fi module disabled${NC}"
+for service in "${services[@]}"; do
+  setup_service "$service"
+done
 
-echo -e "${GREEN}Config restart_services_on_client...${NC}"
-echo "pi ALL=(ALL) NOPASSWD: /home/pi/caesar_project/restart_services_on_server.sh" | tee -a /etc/sudoers || { echo -e "${RED}Failed to config sudoers${NC}"; exit 1; }
+echo -e "${GREEN}Disabling Wi-Fi module...${NC}"
+modprobe -r xradio_wlan
+echo "blacklist xradio_wlan" | tee -a /etc/modprobe.d/blacklist.conf
+update-initramfs -u
 
-echo -e "${GREEN}Config web_config_server.service...${NC}"
-cp ./web_config_server.service /etc/systemd/system/ || { echo -e "${RED}Failed to copy web_config.service${NC}"; exit 1; }
-systemctl daemon-reload || { echo -e "${RED}Failed to reload systemd daemon${NC}"; exit 1; }
-systemctl start web_config_server.service || { echo -e "${RED}Failed to start web_config.service${NC}"; exit 1; }
-systemctl enable web_config_server.service || { echo -e "${RED}Failed to enable web_config.service${NC}"; exit 1; }
-echo -e "${GREEN}web_config_server.service started and enabled${NC}"
+sudoers_entry="pi ALL=(ALL) NOPASSWD: /home/pi/caesar_project/restart_services_on_server.sh"
+grep -qF "$sudoers_entry" /etc/sudoers || 
+  { echo "$sudoers_entry" | sudo EDITOR='tee -a' visudo; }
 
-echo -e "${GREEN}Config alsa_restore.service...${NC}"
-cp ./alsa_restore.service /etc/systemd/system/ || { echo -e "${RED}Failed to copy alsa_restore.service${NC}"; exit 1; }
-systemctl daemon-reload || { echo -e "${RED}Failed to reload systemd daemon${NC}"; exit 1; }
-systemctl start alsa_restore.service
-systemctl enable alsa_restore.service
-echo -e "${GREEN}alsa_restore.service started and enabled${NC}"
-
-echo -e "${GREEN}Done. You need to edit ${RED}client_ip.cfg and server_ip.cfg${GREEN} and reboot (sudo reboot).${NC}"
+echo -e "${GREEN}Configuration completed successfully. Please edit client_ip.cfg and server_ip.cfg, then reboot (sudo reboot).${NC}"
